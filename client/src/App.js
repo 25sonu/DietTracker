39350cae9e7f4f5907471c1f5da349761ed66347
25sonu jena <pratibhajena288@gmail.com>
2024-12-06 04:43:03 +0000

    created footer and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Box } from '@mui/material';
+//import rosePineTheme from './theme/rosepine';
 
-function App() {
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+// import CreateBook from './components/CreateBook';
+// import ShowBookList from './components/ShowBookList';
+// import ShowBookDetails from './components/ShowBookDetails';
+// import UpdateBookInfo from './components/UpdateBookInfo';
+// import HomePage from './components/HomePage';
+// import NotesPage from './components/NotesPage'; // Import NotesPage component
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+       <Box display="flex" flexDirection="column" minHeight="100vh">
+       <Navbar />
+        <Box component="main" flexGrow={1} py={3}>
+      </Box>
+      < Footer/>
+      </Box>
+     </Router>
   );
-}
+};
 
 export default App;
