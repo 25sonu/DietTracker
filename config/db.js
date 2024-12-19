41b4e9f33571fed2c328fc795eb8da58688f0ab9
25sonu jena <pratibhajena288@gmail.com>
2024-12-19 07:41:52 +0000

    the upgraded env

--- config/db.js
+++ config/db.js
@@ -1,17 +1,33 @@
+// const mongoose = require ('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         mongoose.set("strictQuery", true)
+//         await mongoose.connect( 'mongodb+srv ://diet_tracker_admin:diet_tracker_admin@diet-tracker.xvfvs.mongodb.net/?retryWrites=true&w=majority&appName=diet-tracker');
+//         console.log('Connected to MongoDB');
+//     }
+
+//      catch (err) {
+//         console.error(err.message)
+//         console.log("FIX THE DB CONNECTIVITY CODE OR MONGODB URL/USERNAME/PASSWORD!!")
+//        // process.exit(1);
+//     }
+// };
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require ('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.set("strictQuery", true)
-        await mongoose.connect({  path: "./config.env" });
+        await mongoose.connect('mongodb://localhost:27017/mydatabase');
         console.log('Connected to MongoDB');
-    }
-
-     catch (err) {
-        console.error(err.message)
-        console.log("FIX THE DB CONNECTIVITY CODE OR MONGODB URL/USERNAME/PASSWORD!!")
-       // process.exit(1);
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
     }
 };
 
-module.exports = connectDB;
+module.exports = connectDB
